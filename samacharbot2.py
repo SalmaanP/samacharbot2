9134d491c7ecee8aaff208cda62369e2657b4cf5
Salmaan Pehlari <tornadoatindia@gmail.com>
2015-06-22 06:20:20 +0530

    init

--- samacharbot2.py
+++ samacharbot2.py
@@ -1,5 +1,6 @@
 __author__ = 'salmaan'
 
+import sys
 from goose import Goose
 import praw
 import smrzr
@@ -7,3 +8,4 @@ link="http://www.hindustantimes.com/bollywood/peepli-live-co-director-mahmood-fa
 summ_article = smrzr.Summarizer(link)
 summary = summ_article.summary
 print summary
+sys.stdout.flush()
