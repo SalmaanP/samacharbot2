b1147691084ae852a92446d6445e2623ecacdf13
Salmaan Pehlari <tornadoatindia@gmail.com>
2015-06-22 07:15:31 +0530

    still init

--- samacharbot2.py
+++ samacharbot2.py
@@ -6,6 +6,6 @@ import praw
 import smrzr
 link="http://www.hindustantimes.com/bollywood/peepli-live-co-director-mahmood-faaroqui-arrested-on-rape-charges/article1-1361263.aspx"
 summ_article = smrzr.Summarizer(link)
-summary = summ_article.summary
-print summary
+summaryy = summ_article.summary
+print summaryy
 sys.stdout.flush()
